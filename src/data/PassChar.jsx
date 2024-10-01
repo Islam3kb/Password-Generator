511794db87515a40675429c8da3d75da3235984c
Islam3kb <114409595+Islam3kb@users.noreply.github.com>
2024-10-01 12:30:54 +0500

    Password generator

---
+++ src/data/PassChar.jsx
@@ -0,0 +1,5 @@
+// import React from "react";
+export let SC = "!@#$%^&*().";
+export let UC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export let LC = "abcdefghijklmnopqrstuvwxyz";
+export let NC = "0213213312";
